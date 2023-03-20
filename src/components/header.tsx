568b5d6ec176ceb83b972fa792a78497e3aa4d33
woosang <woosang3078@hanmail.net>
2023-03-20 22:47:32 +0900

    ✨ :: 헤더 퍼블리싱

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,55 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import MainLogo from "../assets/svgs/header_logo.svg";
+import SearchLogo from "../assets/svgs/search_logo.svg";
+import MyPageLogo from "../assets/svgs/mypage_logo.svg";
 
 const Header = () => {
-    return (
-        <>
-            <div>
-
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <HeaderContainer>
+          <img src={MainLogo} alt="로고 이미지" />
+          <CategoryContainer>
+            <Categries>홈</Categries>
+            <Categries>커뮤니티</Categries>
+            <Categries>정보수정</Categries>
+            <Categries>고객지원</Categries>
+          </CategoryContainer>
+          <Right>
+            <img src={SearchLogo} alt="검색 로고" />
+            <img src={MyPageLogo} alt="마이페이지 로고" />
+          </Right>
+        </HeaderContainer>
+      </div>
+    </>
+  );
+};
 
 export default Header;
 
+const Categries = styled.li`
+  font-size: 15px;
+`;
+
+const CategoryContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  width: 521px;
+`;
+
+const HeaderContainer = styled.nav`
+  height: 48px;
+  border-bottom: 1px solid rgba(222, 222, 222, 1);
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const Right = styled.div`
+  width: 93px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 45px;
+`;
