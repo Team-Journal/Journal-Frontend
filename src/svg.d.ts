a8d101e0bc44e2c03aa0b43f0b8d4ecc6a1ecdd9
woosang <woosang3078@hanmail.net>
2023-03-20 22:46:02 +0900

    🐛 :: svg 모듈 버그 수정

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,4 +1,4 @@
-declare module '*.svg' {
-    const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default value;
-}
+declare module "*.svg" {
+    const content: any;
+    export default content;
+  }
