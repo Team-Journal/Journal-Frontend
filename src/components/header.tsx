72a76627d0715b69fb29726503ac4d4cf4b95d2a
woosang <woosang3078@hanmail.net>
2023-03-20 19:07:45 +0900

    ✨ :: 헤더 추가

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default Header;
+
