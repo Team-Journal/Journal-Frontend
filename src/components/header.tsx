7ce10bcb8c48e42de25785cadb88d2452c1aad06
woosang <woosang3078@hanmail.net>
2023-03-21 11:27:16 +0900

    💄 :: 헤더 스크롤 고정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,21 +6,19 @@ import MyPageLogo from "../assets/svgs/mypage_logo.svg";
 const Header = () => {
   return (
     <>
-      <div>
-        <HeaderContainer>
-          <img src={MainLogo} alt="로고 이미지" />
-          <CategoryContainer>
-            <Categries>홈</Categries>
-            <Categries>커뮤니티</Categries>
-            <Categries>정보수정</Categries>
-            <Categries>고객지원</Categries>
-          </CategoryContainer>
-          <Right>
-            <img src={SearchLogo} alt="검색 로고" />
-            <img src={MyPageLogo} alt="마이페이지 로고" />
-          </Right>
-        </HeaderContainer>
-      </div>
+      <HeaderContainer>
+        <img src={MainLogo} alt="로고 이미지" />
+        <CategoryContainer>
+          <Categries>홈</Categries>
+          <Categries>커뮤니티</Categries>
+          <Categries>정보수정</Categries>
+          <Categries>고객지원</Categries>
+        </CategoryContainer>
+        <Right>
+          <img src={SearchLogo} alt="검색 로고" />
+          <img src={MyPageLogo} alt="마이페이지 로고" />
+        </Right>
+      </HeaderContainer>
     </>
   );
 };
@@ -39,11 +37,14 @@ const CategoryContainer = styled.ul`
 `;
 
 const HeaderContainer = styled.nav`
+  width: 100%;
   height: 48px;
   border-bottom: 1px solid rgba(222, 222, 222, 1);
   display: flex;
   align-items: center;
   justify-content: space-evenly;
+  position: fixed;
+  background-color: white;
 `;
 
 const Right = styled.div`
