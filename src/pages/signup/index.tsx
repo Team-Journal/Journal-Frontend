8d710b29781305c1f754c001b34eb8dbd2e91f72
woosang <woosang3078@hanmail.net>
2023-03-22 16:52:14 +0900

    ✨ :: 회원가입 페이지 퍼블리싱

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import styled from 'styled-components';
+import MainHeader from '../../components/main-header';
+import text from '../../assets/svgs/text_logo.svg';
+import SignupForm from '../../components/signup';
 
 const Signup = () => {
   return (
     <div>
-      
+      <MainHeader />
+      <Container>
+        <TextImage src={text} alt="텍스트" />
+        <Desc>공유몽에 가입하면 더 많은 혜택을 누릴 수 있어요!</Desc>
+      </Container>
+      <FormContainer>
+        <SignupForm />
+      </FormContainer>
     </div>
   )
 }
 
 export default Signup;
+
+const FormContainer = styled.div`
+  display: flex;
+  margin-top: 300px;
+  justify-content: center;
+`
+
+const Desc = styled.p`
+  font-size: 20px;
+`
+
+const TextImage = styled.img`
+  margin-top: 129px;
+`
+
+const Container = styled.div`
+  width: 392px;
+  height: 75px;
+  margin: 0 auto;
+`
