02452e04b9c8977c3bc4f0f1b00a8e0e336a80e0
woosang <woosang3078@hanmail.net>
2023-03-22 09:27:35 +0900

    ✨ :: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import Community from './pages/community';
 import Introduce from './pages/introduce';
 
 const App: React.FC = () => {
@@ -8,6 +9,7 @@ const App: React.FC = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Introduce />}></Route>
+                <Route path="/community" element={<Community />}></Route>
             </Routes>
         </BrowserRouter>
     )
