d8f87a2239e92eec4adb6743e0d7cfc51b168f41
woosang <woosang3078@hanmail.net>
2023-03-23 10:06:47 +0900

    ✨ :: 로그인 페이지 퍼블리싱

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import styled from 'styled-components'
+import LoginForm from '../../components/login';
+import MainHeader from '../../components/main-header'
 
 const Login = () => {
   return (
     <div>
-      
+      <MainHeader />
+      <Container>
+        <LoginTitle>
+          공유몽에 로그인하기
+        </LoginTitle>
+        <FormContainer>
+          <LoginForm />
+        </FormContainer>
+      </Container>
     </div>
   )
 }
 
-export default Login
+export default Login;
+
+const FormContainer = styled.div`
+  display: flex;
+  margin-top: 30px;
+  justify-content: center;
+`
+
+const LoginTitle = styled.p`
+  font-size: 25px;
+  margin-top: 300px;
+`
+
+const Container = styled.div`
+  display: flex;
+  width: 214px;
+  height: 29px;
+  margin: 0 auto;
+  flex-direction: column;
+`
