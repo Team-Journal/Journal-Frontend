21ddc87dec27e580a1f3a1a40253ac7ce31bc7aa
woosang <woosang3078@hanmail.net>
2023-03-24 23:51:48 +0900

    ✨ :: 커뮤니티 페이지 퍼블리싱

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import MainHeader from "../../components/main-header";
+import SubHeader from "../../components/sub-header";
+import styled from "styled-components";
 
 const Community = () => {
   return (
     <div>
-      
+      <MainHeader />
+      <SubHeader
+        title="커뮤니티"
+        menu1="커뮤니티에 업로드"
+        menu2="둘러보기"
+        menu3="아이디 검색"
+      />
+      <Container>
+        <PageTitle>커뮤니티 둘러보기</PageTitle>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Community;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const PageTitle = styled.p`
+  font-size: 35px;
+  margin-top: 200px;
+  font-weight: 650;
+  margin-left: 245px;
+  display: flex;
+`;
