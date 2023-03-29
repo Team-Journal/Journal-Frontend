464e0c5c48d5758b30bfcf25c47fbb8e1ac85588
woosang <woosang3078@hanmail.net>
2023-03-29 10:04:10 +0900

    📝 :: 회원가입 로직 추가

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,10 +1,39 @@
 import styled from "styled-components";
+import { SignupFormData } from "../types/type";
+import { useState } from "react";
 
 const SignupForm = () => {
+  const [formData, setFormData] = useState<SignupFormData>({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    verifyCode: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    // 회원가입 처리 로직 구현 예정
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <EmailContainer>
-        <EmailInput placeholder="name@example.com" type="email" />
+        <EmailInput
+          placeholder="name@example.com"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
         <CodeSubmitBtn>코드 발송</CodeSubmitBtn>
       </EmailContainer>
       <Container>
@@ -13,14 +42,26 @@ const SignupForm = () => {
           type="number"
           autoFocus
           required
+          name="verifyCode"
+          value={formData.verifyCode}
+          onChange={handleInputChange}
         />
-        <PasswordInput placeholder="비밀번호를 입력해주세요." type="password" />
+        <PasswordInput
+          placeholder="비밀번호를 입력해주세요."
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
         <PasswordCheckInput
           placeholder="비밀번호를 다시 입력해주세요."
           type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleInputChange}
         />
         <BtnContainer>
-            <ContinueBtn>계속</ContinueBtn>
+          <ContinueBtn type="submit">계속</ContinueBtn>
         </BtnContainer>
       </Container>
     </form>
@@ -30,23 +71,23 @@ const SignupForm = () => {
 export default SignupForm;
 
 const BtnContainer = styled.div`
-    width: 101px;
-    height: 55px;
-    margin: 0 auto;
-`
+  width: 101px;
+  height: 55px;
+  margin: 0 auto;
+`;
 
 const ContinueBtn = styled.button`
-    width: 101px;
-    height: 55px;
-    background-color: rgba(171, 178, 239, 1);
-    font-size: 20px;
-    border-radius: 7px;
-    color: white;
-    border: none;
-    font-weight: 600;
-    margin-top: 100px;
-    cursor: pointer;
-`
+  width: 101px;
+  height: 55px;
+  background-color: rgba(171, 178, 239, 1);
+  font-size: 20px;
+  border-radius: 7px;
+  color: white;
+  border: none;
+  font-weight: 600;
+  margin-top: 100px;
+  cursor: pointer;
+`;
 
 const PasswordCheckInput = styled.input`
   width: 492px;
