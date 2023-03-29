2ace56a91f626090baa8649d826e07fefbf855ec
woosang <woosang3078@hanmail.net>
2023-03-29 09:29:58 +0900

    📝 :: 회원가입 폼 데이터 타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -4,3 +4,10 @@ export interface HeaderProps {
     menu2: string
     menu3: string
 }
+
+export interface SignupFormData {
+    email: string
+    password: string
+    confirmPassword: string
+    verifyCode: string
+}
