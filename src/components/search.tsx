c22b1ef25f8ffc5424ffced8f9875b5227e58a31
woosang <woosang3078@hanmail.net>
2023-04-04 11:58:02 +0900

    📝 :: 입력 시 버튼 생성

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,59 +1,75 @@
-import styled from 'styled-components';
-import search from '../assets/svgs/id_search.svg';
+import { useState } from "react";
+import styled from "styled-components";
+import search from "../assets/svgs/id_search.svg";
 
 const SearchForm = () => {
-    return (
-        <form>
-            <Container>
-                <InputContainer>
-                  <SearchImage src={search} alt="검색 로고" />
-                  <NicknameInput placeholder="아이디 검색" />
-                </InputContainer>
-                <SearchBtn>검색</SearchBtn>
-            </Container>
-        </form>
-    )
-}
+  const [isInput, setIsInput] = useState<boolean>(false);
+  const [nickname, setNickname] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+
+    if (nickname === "") {
+      setIsInput(false);
+    }
+
+    if (nickname.length >= 0) {
+        setIsInput(true);
+    }
+  };
+
+  return (
+    <form>
+      <Container>
+        <InputContainer>
+          <SearchImage src={search} alt="검색 로고" />
+          <NicknameInput placeholder="아이디 검색" onChange={handleChange} />
+        </InputContainer>
+        {isInput && <SearchBtn>검색</SearchBtn>}
+      </Container>
+    </form>
+  );
+};
 
 export default SearchForm;
 
 const SearchBtn = styled.button`
-    width: 108px;
-    height: 65px;
-    background-color: rgba(171, 178, 239, 1);
-    border-radius: 20px;
-    border: none;
-    font-size: 20px;
-    font-weight: 600;
-    color: white;
-    margin-left: 20px;
-`
+  width: 108px;
+  height: 65px;
+  background-color: rgba(171, 178, 239, 1);
+  border-radius: 20px;
+  border: none;
+  font-size: 20px;
+  font-weight: 600;
+  color: white;
+  margin-left: 20px;
+`;
 
 const NicknameInput = styled.input`
-    width: 700px;
-    height: 58px;
-    border-radius: 10px;
-    display: flex;
-    border: none;
-    outline: none;
-    font-size: 18px;
-`
+  width: 700px;
+  height: 58px;
+  border-radius: 10px;
+  display: flex;
+  border: none;
+  outline: none;
+  font-size: 18px;
+`;
 
 const SearchImage = styled.img`
-    display: flex;
-    padding: 10px;
-`
+  display: flex;
+  padding: 10px;
+`;
 
 const InputContainer = styled.div`
-    width: 750px;
-    height: 60px;
-    border: 1px solid rgba(166, 166, 166, 1);
-    border-radius: 10px;
-    display: flex;
-`
+  width: 750px;
+  height: 60px;
+  border: 1px solid rgba(166, 166, 166, 1);
+  border-radius: 10px;
+  display: flex;
+`;
 
 const Container = styled.div`
-    width: 878px;
-    height: 72px;
-    display: flex;
-`
+  width: 878px;
+  height: 72px;
+  display: flex;
+`;
