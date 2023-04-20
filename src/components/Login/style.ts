01e5407cecd1010aa5fd96f8a902f5edbc514bf2
woosang <woosang3078@hanmail.net>
2023-04-20 20:18:35 +0900

    💄 :: 스타일 파일 추가

---
+++ src/components/Login/style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const ConfirmBtn = styled.img`
+  margin-left: 20px;
+`
+
+export const ConfirmBtnContainer = styled.div`
+  display: flex;
+`;
+
+export const PasswordInput = styled.input`
+  width: 270px;
+  height: 70px;
+  border: none;
+  border-radius: 25px;
+  font-size: 20px;
+  padding-left: 27px;
+  outline: none;
+`;
+
+export const EmailInput = styled.input`
+  width: 300x;
+  height: 70px;
+  border-bottom: 1px solid rgba(198, 198, 198, 1);
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-radius: 25px;
+  font-size: 20px;
+  padding-left: 27px;
+  outline: none;
+`;
+
+export const InputContaier = styled.div`
+  width: 392px;
+  height: 146px;
+  border: 1.2px solid rgba(198, 198, 198, 1);
+  display: flex;
+  border-radius: 25px;
+  flex-direction: column;
+`;
